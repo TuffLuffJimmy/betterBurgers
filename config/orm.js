6dc6d12b8d48fa17d5c99a8f119580fbe00a76ff
Jimmy Echternach <jimmynechternach@gmail.com>
2020-05-29 22:31:03 -0700

    updated

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+const db = require('../config/connection.js')
+
+const insertOne = () => {
+
+}
+
+const updateOne = () => {
+
+}
+
+const selectAll = () => {
+  db.query('SELECT * FROM burgers')
+}
