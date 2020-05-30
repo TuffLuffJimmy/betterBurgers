7e879635e049a3da5d362ccf43e0f81e3b83042d
Jimmy Echternach <jimmynechternach@gmail.com>
2020-05-29 23:03:16 -0700

    heroku and jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,16 @@
 const mysql = require('mysql2')
+let connection
 
-module.exports = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'rootroot',
-  database: 'burgers_db'
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'rootroot',
+    database: 'burgers_db'
+  })
+}
+
+module.exports = connection
--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,15 @@
 const db = require('../config/connection.js')
 
 const insertOne = () => {
-
+  db.query('INSERT INTO burgers SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
 }
 
 const updateOne = () => {
-
+  db.query('INSERT INTO burgers SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
 }
 
 const selectAll = () => {
